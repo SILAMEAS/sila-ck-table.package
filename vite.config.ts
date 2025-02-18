cc34a5c5f8e1c4e12244d74c2bcec5d4e8ad8b87
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2025-02-18 23:27:20 +0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/sila-ck-notification-npm",
+  base:"/sila-ck-notification-npm/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
