2398416d4f19ea995212fa116d5e523e84696b62
Sila MEAS <sila.meas@allweb.com.kh>
2025-06-20 11:15:14 +0700

    type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-export * from './components/CellCustom'
-export * from './components/TableCustom'
-export * from './constant/enum/EnumTableFooterType'
-export * from './utils/handleProcessPassingData'
-export * from './constant/localTableCellProps'
-export * from './hooks/useTableCustom'
+import CellCustom from './components/CellCustom'
+import TableCustom from './components/TableCustom'
+import EnumTableFooterType from './constant/enum/EnumTableFooterType'
+import handleProcessPassingData from './utils/handleProcessPassingData'
+import localTableCellProps from './constant/localTableCellProps'
+import useTableCustom from './hooks/useTableCustom'
+
+export {
+    CellCustom,
+    TableCustom,
+    EnumTableFooterType,
+    handleProcessPassingData,
+    localTableCellProps,
+    useTableCustom,
+}
