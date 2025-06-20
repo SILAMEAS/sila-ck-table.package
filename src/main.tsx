bb83a04cc736b2fe12de8bf0cec9e541120d86fb
Sila MEAS <sila.meas@allweb.com.kh>
2025-06-20 10:58:36 +0700

    add script

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// npm version patch   # for bugfixes
+// npm version minor   # for new features
+// npm version major   # for breaking changes
+
 createRoot(document.getElementById('root')!).render(
   <App />
 )
